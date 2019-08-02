f5002d249dbdaec9adcd43ef03f8466796919d3d
Zhuang, Rong <jojozhuang@gmail.com>
2019-08-02 15:39:20 -0700

    move custom list to practice problem

--- java-list/src/johnny/java/common/IQueue.java
+++
@@ -1,9 +0,0 @@
-package johnny.java.common;
-
-public interface IQueue<E> {
-    boolean offer(E e);
-    E poll();
-    E peek();
-    int size();
-    boolean isEmpty();
-}
--- java-list/src/johnny/java/common/IStack.java
+++
@@ -1,9 +0,0 @@
-package johnny.java.common;
-
-public interface IStack<E> {
-    void push(E e);
-    E pop();
-    E peek();
-    int size();
-    boolean isEmpty();
-}
