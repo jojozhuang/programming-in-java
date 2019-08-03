a217e053f83acab6990d769681316db17aa57895
Zhuang, Rong <jojozhuang@gmail.com>
2019-08-02 21:53:48 -0700

    refine predicate

--- java-java8-predicate/src/johnny/java8/predicate/PredicateExample.java
+++ java-java8-predicate/src/johnny/java8/predicate/PredicateExample.java
@@ -1,10 +1,10 @@
-package johnny.java.functional;
+package johnny.java8.predicate;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class PredicateTest {
+public class PredicateExample {
     public static void main(String args[]) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
